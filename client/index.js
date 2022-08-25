1425a8b49bd013e874772fd1c505026824e13c95
daniel-chowdhury <danielfchowdhury@gmail.com>
2022-08-25 00:08:34 -0400

    ball is moving, goalpost element moving, handling collision

--- client/index.js
+++ client/index.js
@@ -1,16 +1,17 @@
 let goalpost_velocity = 0.25
-
+// ----------------------------------------------------------------------------------------------------------
 class Ball {
 constructor(ball_element) {
     this.ball_element = ball_element
 }
 
-update(time_delta) {
+update() {
     this.x += 0.25
 }
 
 reset() {
-
+    this.x = 10
+    this.y = 50
 }
 
 get x() {
@@ -34,13 +35,13 @@ rect() {
 }
 
 }
-
+// ----------------------------------------------------------------------------------------------------------
 class Goalpost {
     constructor(goalpost_element) {
         this.goalpost_element = goalpost_element
     }
 
-    update(time_delta) {
+    update() {
         this.location += goalpost_velocity 
         let rect = this.rect()
         if (rect.bottom >= window.innerHeight || rect.top <= 0) {
@@ -62,31 +63,31 @@ class Goalpost {
     }
     
 }
-
+// ----------------------------------------------------------------------------------------------------------
 let ball = new Ball(document.getElementById("ball"))
 let goalpost = new Goalpost(document.getElementById("goalpost"))
 
 
-let last_time
+
 function update_ball(time) {
-    let last_time
-    if (last_time !== null) {
-        let delta_time = time - last_time
-        ball.update(delta_time)
+    let ball_rect = ball.rect()
+    let goalpost_rect = goalpost.rect()
+    if ((goalpost_rect.left > ball_rect.right
+        || (goalpost_rect.bottom < ball_rect.top || goalpost_rect.top > ball_rect.bottom))
+        && ball_rect.right < window.innerWidth) {
+        ball.update()
+        window.requestAnimationFrame(update_ball)
+    } else {
+        ball.reset()
     }
-    last_time = time
-    window.requestAnimationFrame(update_ball)
+    
 }
 
-let last_time2
+
 
 function update_goalpost(time) {
-    console.log("matrix")
-    if (last_time2 !== null) {
-        let delta_time = time - last_time2
-        goalpost.update(delta_time)
-    }
-    last_time2 = time
+
+    goalpost.update()
     window.requestAnimationFrame(update_goalpost)
 }
 
@@ -94,7 +95,9 @@ window.requestAnimationFrame(update_goalpost)
 
 document.getElementById("launch_button").addEventListener("click", (event) => {
     event.preventDefault()
+    if (ball.x < 12) {
     window.requestAnimationFrame(update_ball)
+    }
 })
 
 let input = document.querySelector("input")
